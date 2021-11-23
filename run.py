928cc0738fe2f5ee78326e0b95101ee05ab7efb4
Moritz Makowski <moritz.makowski@tum.de>
2021-11-23 17:57:39 +0000

    Extract stdout keywords

--- run.py
+++ run.py
@@ -10,6 +10,8 @@ with open(f"{PROJECT_DIR}/config.json", "r") as f:
 
 BACKOFF_TIMES = config["backoff_times"]
 JOB_COMMAND = config["job_command"]
+SHORT_DELAY_KEYWORD = config["keyword_for_short_delay"]
+LONG_DELAY_KEYWORD = config["keyword_for_long_delay"]
 STATE_FILE = f"{PROJECT_DIR}/state.json"
 REPORT_FILE = f"{PROJECT_DIR}/report.txt"
 
@@ -21,20 +23,20 @@ if not os.path.isfile(STATE_FILE):
 # read last backoff index from STATE_FILE
 with open(STATE_FILE) as f:
     config = json.load(f)
-    last_backoff_index = config["last_backoff_index"]
-    assert last_backoff_index in range(len(BACKOFF_TIMES))
+    backoff_index = config["last_backoff_index"]
+    assert backoff_index in range(len(BACKOFF_TIMES))
 
 # run the command and read the stdout
 result = subprocess.run(JOB_COMMAND.split(), stdout=subprocess.PIPE)
 stdout = result.stdout.decode()
 
 # determine the next backoff time based on stdout
-if last_backoff_index < len(BACKOFF_TIMES) - 1:
-    new_backoff_index = last_backoff_index + 1
-if "finished" in stdout:
-    new_backoff_index = 0
-elif "failed" in stdout:
-    new_backoff_index = len(BACKOFF_TIMES) - 1
+if backoff_index < len(BACKOFF_TIMES) - 1:
+    backoff_index += 1
+if SHORT_DELAY_KEYWORD in stdout:
+    backoff_index = 0
+elif LONG_DELAY_KEYWORD in stdout:
+    backoff_index = len(BACKOFF_TIMES) - 1
 
 # append timestamp + stdout to REPORT_FILE
 with open(REPORT_FILE, "a") as f:
@@ -43,10 +45,10 @@ with open(REPORT_FILE, "a") as f:
 
 # save the state for the next execution
 with open(STATE_FILE, "w") as f:
-    json.dump({"last_backoff_index": new_backoff_index}, f)
+    json.dump({"last_backoff_index": backoff_index}, f)
 
 # start next execution at "now + backoff time"
 os.system(
     f'echo "python3 {PROJECT_DIR}/run.py" '
-    + f"| at now + {BACKOFF_TIMES[new_backoff_index]}"
+    + f"| at now + {BACKOFF_TIMES[backoff_index]}"
 )
