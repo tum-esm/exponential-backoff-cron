8857181e78a4286b55ef5a2e48a1bc2fd604c1ec
Moritz Makowski <moritz.makowski@tum.de>
2021-11-23 17:28:10 +0000

    Use config.json file

--- run.py
+++ run.py
@@ -4,34 +4,49 @@ import json
 import subprocess
 from datetime import datetime
 
-backoff_times = ["1 minutes", "4 minutes", "12 minutes", "2 hours", "24 hours"]
-project_dir = "/home/moritz/remote-cron"
-job_command = f"bash /home/moritz/execute-remote-job.sh"
-state_file = f"{project_dir}/state.json"
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+with open(f"{PROJECT_DIR}/config.json", "r") as f:
+    config = json.load(f)
 
-if not os.path.isfile(state_file):
-    with open(f"{project_dir}/state.json", "w") as f:
+BACKOFF_TIMES = config["backoff_times"]
+JOB_COMMAND = config["job_command"]
+STATE_FILE = f"{PROJECT_DIR}/state.json"
+REPORT_FILE = f"{PROJECT_DIR}/report.txt"
+
+# create STATE_FILE if it does not exist yet
+if not os.path.isfile(STATE_FILE):
+    with open(STATE_FILE, "w") as f:
         json.dump({"last_backoff_index": 0}, f)
 
-with open(f"{project_dir}/state.json") as f:
+# read last backoff index from STATE_FILE
+with open(STATE_FILE) as f:
     config = json.load(f)
     last_backoff_index = config["last_backoff_index"]
-    assert last_backoff_index in range(len(backoff_times))
+    assert last_backoff_index in range(len(BACKOFF_TIMES))
 
-result = subprocess.run(job_command.split(), stdout=subprocess.PIPE)
+# run the command and read the stdout
+result = subprocess.run(JOB_COMMAND.split(), stdout=subprocess.PIPE)
 stdout = result.stdout.decode()
 
-new_backoff_index = min(last_backoff_index + 1, len(backoff_times) - 1)
+# determine the next backoff time based on stdout
+if last_backoff_index < len(BACKOFF_TIMES) - 1:
+    new_backoff_index = last_backoff_index + 1
 if "finished" in stdout:
     new_backoff_index = 0
+elif "failed" in stdout:
+    new_backoff_index = len(BACKOFF_TIMES) - 1
 
-with open(f"{project_dir}/report.txt", "a") as f:
+# append timestamp + stdout to REPORT_FILE
+with open(REPORT_FILE, "a") as f:
     f.write(str(datetime.now()) + "\n")
     f.write(stdout + "\n")
 
-with open(f"{project_dir}/state.json", "w") as f:
+# save the state for the next execution
+with open(STATE_FILE, "w") as f:
     json.dump({"last_backoff_index": new_backoff_index}, f)
 
+# start next execution at "now + backoff time"
 os.system(
-    f'echo "python3 {project_dir}/run.py" | at now + {backoff_times[new_backoff_index]}'
+    f'echo "python3 {PROJECT_DIR}/run.py" '
+    + f"| at now + {BACKOFF_TIMES[new_backoff_index]}"
 )
